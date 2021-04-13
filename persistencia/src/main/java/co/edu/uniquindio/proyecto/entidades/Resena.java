4b2a5513eaf51f49ba4a080d3fe360d59a382f93
Cristian Alexander Guevara <caguevarar@uqvirtual.edu.co>
2021-04-13 18:42:32 -0500

    Correccion nombre clases

---
+++ persistencia/src/main/java/co/edu/uniquindio/proyecto/entidades/Resena.java
@@ -0,0 +1,11 @@
+package co.edu.uniquindio.proyecto.entidades;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class Resena {
+
+    @Id
+    private int codigo;
+}
