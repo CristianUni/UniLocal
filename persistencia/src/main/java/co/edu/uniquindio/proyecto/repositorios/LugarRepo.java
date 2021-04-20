52b0f7439168054a9d288c532505ed34daf5eb28
Nicolas Valencia Gallego <nvalenciag_2@uqvirtual.edu.co>
2021-04-19 19:41:25 -0500

    comentarios de las clases Lugar y Categoria ademas de comentar los test de Lugar y Categoria

--- persistencia/src/main/java/co/edu/uniquindio/proyecto/repositorios/LugarRepo.java
+++ persistencia/src/main/java/co/edu/uniquindio/proyecto/repositorios/LugarRepo.java
@@ -2,6 +2,9 @@ package co.edu.uniquindio.proyecto.repositorios;
 
 import co.edu.uniquindio.proyecto.entidades.Lugar;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+//repositorio de lugar con llave integer
+@Repository
 public interface LugarRepo extends JpaRepository<Lugar,Integer> {
 }
